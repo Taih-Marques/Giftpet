59d6456e25205d181f15d11c3029f382ba5650d7
Tainah <tayhmarques.00@gmail.com>
2025-10-27 20:05:43 -0300

    Initial commit

---
+++ src/main/java/com/giftpet/giftpet/GiftpetApplication.java
@@ -0,0 +1,13 @@
+package com.giftpet.giftpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftpetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftpetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/giftpet/giftpet/GiftpetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giftpet.giftpet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftpetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
