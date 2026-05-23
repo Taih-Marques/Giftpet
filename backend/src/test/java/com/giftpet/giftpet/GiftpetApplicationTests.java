cb348b0975bd6e29dc687c0caef076f021846b77
Tainah Marques <tayhmarques.00@gmail.com>
2026-05-23 17:12:11 -0300

    config de dev e testes

--- backend/src/test/java/com/giftpet/giftpet/GiftpetApplicationTests.java
+++ backend/src/test/java/com/giftpet/giftpet/GiftpetApplicationTests.java
@@ -2,8 +2,10 @@ package com.giftpet.giftpet;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GiftpetApplicationTests {
 
 	@Test
